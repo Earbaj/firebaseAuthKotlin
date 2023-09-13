f5df1c72a49121934c42df35bc1a98cadc627e1e
earba <earbajsaria3@gmail.com>
2023-09-13 16:17:26 +0600

    Design login and registeraration page add dependancy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
     implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/firebaseauthinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauthinkotlin/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.firebaseauthinkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firebaseauthinkotlin.databinding.ActivityMainBinding
+import com.example.firebaseauthinkotlin.ui.LoginPage
+import com.example.firebaseauthinkotlin.ui.RegisterPage
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +16,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener {
+            startActivity(Intent(this, LoginPage::class.java))
+        }
+
+        binding.btnRegistration.setOnClickListener {
+            startActivity(Intent(this, RegisterPage::class.java))
+        }
+
     }
 }
